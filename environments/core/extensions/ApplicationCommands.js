9f5325442e58cb88f8c172843418f1a9bb82ca8c
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-08-16 17:20:21 +0545

    refactor(Extensions): individual handler

--- environments/core/extensions/ApplicationCommands.js
+++ environments/core/extensions/ApplicationCommands.js
@@ -22,40 +22,6 @@ module.exports = (mainWindow) => {
         if (mainWindow.closable) mainWindow.close();
     });
 
-    ipcMain.on("recentWorkspace", (event) => {
-        const data = db.get("recentWorkspace", []);
-        const workspaces = [];
-
-        for (const item of data) {
-            if (existsSync(item) && statSync(item).isDirectory()) workspaces.push(item);
-        }
-
-        event.reply("recentWorkspace", workspaces);
-    });
-
-    ipcMain.on("openWorkspace", async (event) => {
-        const response = await dialog
-            .showOpenDialog(mainWindow, {
-                title: "Scratch For Discord",
-                properties: ["dontAddToRecent", "openDirectory"]
-            })
-            .then(
-                (data) => {
-                    if (data.canceled) return null;
-                    return data.filePaths;
-                },
-                () => null
-            );
-
-        if (response != null) {
-            const lastData = db.get("recentWorkspace", []);
-            lastData.unshift(...response);
-            db.set("recentWorkspace", [...new Set(lastData)].slice(0, 5));
-        }
-
-        event.reply("openWorkspace", response || []);
-    });
-
     ipcMain.on("saveAs", async (event) => {
         const response = await dialog
             .showSaveDialog(mainWindow, {
@@ -87,26 +53,6 @@ module.exports = (mainWindow) => {
                 event.reply("readFile", res);
             })
             .catch(() => event.reply("readFile", null));
-    });
-
-    ipcMain.on("toggleRPC", (event, enable) => {
-        const mode = Boolean(enable);
-        db.set("rpcEnabled", mode);
-        if (mode) {
-            ipcMain.emit("setActivity");
-        } else {
-            ipcMain.emit("destroyRPC");
-        }
-    });
-
-    ipcMain.on("setServer", (event, url) => {
-        const matches = /^(http(s)?):\/\/(deploy-preview-(\d+)--)?scratch-for-discord.netlify.app(\/)?$/.test(url);
-        db.set("scratchServer", matches ? url : "https://scratch-for-discord.netlify.app");
-    });
-
-    ipcMain.on("settings", (event) => {
-        const data = db.all();
-        event.reply("settings", data);
     });
 
     ipcMain.on("connection", async (event, runFallbackServer = false) => {
---
+++ environments/core/extensions/SettingsManager.js
@@ -0,0 +1,61 @@
+const { ipcMain, BrowserWindow } = require("electron");
+const db = require("../storage/database");
+
+/**
+ * @param {BrowserWindow} mainWindow
+ */
+module.exports = (mainWindow) => {
+    ipcMain.on("recentWorkspace", (event) => {
+        const data = db.get("recentWorkspace", []);
+        const workspaces = [];
+
+        for (const item of data) {
+            if (existsSync(item) && statSync(item).isDirectory()) workspaces.push(item);
+        }
+
+        event.reply("recentWorkspace", workspaces);
+    });
+
+    ipcMain.on("openWorkspace", async (event) => {
+        const response = await dialog
+            .showOpenDialog(mainWindow, {
+                title: "Scratch For Discord",
+                properties: ["dontAddToRecent", "openDirectory"]
+            })
+            .then(
+                (data) => {
+                    if (data.canceled) return null;
+                    return data.filePaths;
+                },
+                () => null
+            );
+
+        if (response != null) {
+            const lastData = db.get("recentWorkspace", []);
+            lastData.unshift(...response);
+            db.set("recentWorkspace", [...new Set(lastData)].slice(0, 5));
+        }
+
+        event.reply("openWorkspace", response || []);
+    });
+
+    ipcMain.on("toggleRPC", (event, enable) => {
+        const mode = Boolean(enable);
+        db.set("rpcEnabled", mode);
+        if (mode) {
+            ipcMain.emit("setActivity");
+        } else {
+            ipcMain.emit("destroyRPC");
+        }
+    });
+
+    ipcMain.on("setServer", (event, url) => {
+        const matches = /^(http(s)?):\/\/(deploy-preview-(\d+)--)?scratch-for-discord.netlify.app(\/)?$/.test(url);
+        db.set("scratchServer", matches ? url : "https://scratch-for-discord.netlify.app");
+    });
+
+    ipcMain.on("settings", (event) => {
+        const data = db.all();
+        event.reply("settings", data);
+    });
+};
